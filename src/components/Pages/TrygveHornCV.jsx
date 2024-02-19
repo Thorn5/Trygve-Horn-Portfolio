adf5ae944698324d5303f17e74b7d81979c7223b
Trygve Horn <trygvehorn@gmail.com>
2024-02-19 13:02:36 +0100

    SnapSave

--- src/components/Pages/TrygveHornCV.jsx
+++ src/components/Pages/TrygveHornCV.jsx
@@ -1,13 +1,15 @@
 // TrygveHornCV.jsx
-// import React from 'react'
+import React from 'react'
 
 export const TrygveHornCV = () => {
   return (
     <div>
       <iframe
-        style={{ width: "100%", height: "calc(100vh - 108px)", }}
-        src= {import.meta.env.VITE_APP_CV_PDF}
-        {...console.log(import.meta.env.VITE_APP_CV_PDF)}
+        style={{
+          width: '100%',
+          height: "calc(100vh - 108px)",
+        }}
+        src={import.meta.env.VITE_APP_CV_PDF}
       >
       </iframe>
     </div>
