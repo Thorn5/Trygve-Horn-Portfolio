dde807341b7ee9e77d1c3dbd776a3234d65c95db
Trygve Horn <trygvehorn@gmail.com>
2024-09-20 14:34:09 +0200

    Added theming to language selector

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -2,48 +2,40 @@
 
 import React, { useState } from "react";
 import i18n from '../i18n';
-
-const setStoredData = () => {
-    console.log("setStoredData function triggered, not functional yet");
-    // const [city, setCity] = useState(null);
-    // const [country, setCountry] = useState(null);
-    // const [weather, setWeather] = useState(null);
-
-    // if (sessionStorage.length > 0) {
-    //     const city = sessionStorage.getItem(city);
-    //     // setCity();
-    //     // setCountry();
-    //     // setWeather();
-
-    // } else {
-    //     console.log("setStoredData(): Session Storage empty");
-    // }
-
-
-    // return ();
-};
+import Stack from '@mui/material/Stack';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 export const LanguageSelector = () => {
 
     const [selectedLanguage, setSelectedLanguage] = useState(i18n.language); // i18n.language contains the language assigned to lng in i18n.js file.
-
-
 
     const chooseLanguage = (e) => {
         e.preventDefault();
         i18n.changeLanguage(e.target.value);   // i18n.changeLanguage() is used to change the language assigned to lng in i18n.js file.
         setSelectedLanguage(e.target.value);
         localStorage.setItem("lang", e.target.value);
-        setStoredData();
     }
 
-
-
     return (
-        <select defaultValue={selectedLanguage} onChange={chooseLanguage}>
-            <option value="en">English</option>
-            <option value="de">Deutsch</option>
-        </select>
+        <>
+            {/* <select defaultValue={selectedLanguage} onChange={chooseLanguage}>
+                <option value="en">English</option>
+                <option value="de">Deutsch</option>
+            </select> */}
+            <Stack direction="row">
+                <ToggleButtonGroup
+                    value={selectedLanguage}
+                    exclusive
+                    onChange={chooseLanguage}
+                    aria-label="text alignment">
+                    <ToggleButton value="en" aria-label="English Version">EN
+                    </ToggleButton>
+                    <ToggleButton value="de" aria-label="German Version">DE
+                    </ToggleButton>
+                </ToggleButtonGroup>
+            </Stack>
+        </>
     );
 };
 
